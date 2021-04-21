f9f8c7ad1af849025bfd7277e9d25182558eb85a
george666-cloud <georgejonkjava@gmail.com>
2021-04-21 15:58:57 +0700

    vue-response

---
+++ src/views/transaction/Login.vue
@@ -0,0 +1,21 @@
+<template>
+<h3>login</h3>
+   <div class="container my-5">
+       <div class="row justify-content-center">
+        <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Email address</label>
+</div>
+<div class="form-floating">
+  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+  <label for="floatingPassword">Password</label>
+</div>
+</div>
+<div class="container">
+    <p></p>
+        <div class="col md-5">
+    <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+</div>
+   </div>
+</template>
